db03c743316030b6abb6b46fcb19e4bd1818f52a
EdersonSouza02 <edersonfreitas2012@hotmail.com>
2020-03-01 17:21:39 -0300

    Criação da página lista de eventos

--- demo/src/main/java/com/testeSpring/demo/controller/EventoController.java
+++ demo/src/main/java/com/testeSpring/demo/controller/EventoController.java
@@ -4,25 +4,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.testeSpring.demo.model.Evento;
 import com.testeSpring.demo.repository.EventoRepository;
 
 @Controller
 public class EventoController {
-	
+
 	@Autowired
 	private EventoRepository eventoRepository;
-	
-	
-	@RequestMapping(value="/cadastrarEvento" ,method = RequestMethod.GET)
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String formIndex() {
+
+		return "index.html";
+
+	}
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
 	}
-	@RequestMapping(value="/cadastrarEvento" ,method = RequestMethod.POST)
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
 	public String form(Evento evento) {
-		
+
 		eventoRepository.save(evento);
 		return "redirect:/cadastrarEvento";
+	}
+
+	@RequestMapping("/listaEventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("evento/listaEventos");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+
 	}
 }
